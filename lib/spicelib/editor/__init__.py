035e370d06b4cafe85e701fafb3cbfcc86b58884
Eugene Dann <edanniii@gmail.com>
2025-06-23 13:58:47 -0400

    fixed maybe?

--- lib/spicelib/editor/__init__.py
+++
@@ -1,3 +0,0 @@
-from .qsch_editor import QschEditor
-from .asc_editor import AscEditor
-from .spice_editor import SpiceEditor, SpiceCircuit
--- test.py
+++ test.py
@@ -1,27 +1,29 @@
 from lib.project import SpiceProject
 from lib.sim import Sim
-from lib.themes.theme import PlotTheme
 from lib.utils import metric_notation as mn
 
-import numpy as np
+caps = []
+caps_metric = []
+
+for cap in range(60, 70, 10):
+    c = cap*mn.fm('100p')
+    caps.append(c)
+    caps_metric.append(f'{mn.tm(c)}F')
 
 project = SpiceProject('output', 'test.cir')
 
 # === Transient Analysis ===
-project.sim = Sim.tran('0.1u', '1m', '0')
-project.run()
+project.sim = Sim.tran('0.1u', '100u', '50u')
+for cap in caps:
+    project.component('C1', cap)
+    project.run()
+
+
 project.read_raw()
 
 project.clear_plots()
-project.add_plot('v(TP1)', name='TP1 - Transient')
-project.plot(title='TP1 - Time Domain')
+project.add_plot('V(TP1)', name='TP1 - Transient', labels=caps_metric)
+project.add_plot('v(TP1, TP2)', name='R1 Voltage Drop', labels=caps_metric)
+project.add_plot('v(TP2)', name='R2 Voltage Drop', labels=caps_metric)
 
-# === AC Analysis ===
-project = SpiceProject('output', 'test.cir')
-project.sim = Sim.ac('dec', 100, '1', '2Meg')
-project.run()
-project.read_raw()
-
-project.clear_plots()
-project.add_plot('v(TP1)', name='TP1 - AC Sweep')
-project.plot(title='TP1 - Frequency Domain')
+project.plot(title='Time Domain')
